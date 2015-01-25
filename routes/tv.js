e964d7d42d1fa7f2b88c68a4cd2c04b24ef3964c
theceej <theceej@users.noreply.github.com>
2015-01-25 20:55:04 +0000

    First working version

--- routes/tv.js
+++ routes/tv.js
@@ -4,8 +4,8 @@ var router = express.Router();
 //var bodyParser = require('body-parser');
 //var jsonParser = bodyParser.json();
 
-var SerialPort = require("serialport").SerialPort
-var usbSerial = new SerialPort("/dev/ttyUSB0", {
+var SerialPort = require('serialport').SerialPort
+var usbSerial = new SerialPort('/dev/ttyUSB0', {
   baudrate: 9600
 });
 
@@ -13,12 +13,25 @@ var usbSerial = new SerialPort("/dev/ttyUSB0", {
 
 // Turn TV on
 router.put('/power', function(req, res) {
-  console.log(req.body);
-  //  usbSerial.write("ka 01 01\n", function(err, results) {
-  //    console.log('err ' + err);
-  //    console.log('results ' + results);
-  //  });
-  res.send("Turned it on");
+  if(typeof(req.body.on) === 'undefined') {
+    res.status(400).json({ error : '"on" must be specified'})
+  } else if(req.body.on) {
+    usbSerial.write('ka 01 01\n', function(err, results) {
+      if(err) {
+        res.status(500).json({ error : 'Could not turn on TV: ' + err });
+      } else {
+        res.status(200).json({ on : true });
+      }
+    }
+  } else {
+    usbSerial.write('ka 01 00\n', function(err, results) {
+      if(err) {
+        res.status(500).json({ error : 'Could not turn off TV: ' + err });
+      } else {
+        res.status(200).json({ on : false });
+      }
+    }
+  }
 });
 
 module.exports = router;
