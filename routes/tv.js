da677f53577cc464e542b5cab66d3d0968eb6214
theceej <theceej@users.noreply.github.com>
2015-01-25 20:38:01 +0000

    Now with JSON

--- routes/tv.js
+++ routes/tv.js
@@ -1,20 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+//var bodyParser = require('body-parser');
+//var jsonParser = bodyParser.json();
+
 var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort("/dev/ttyUSB0", {
+var usbSerial = new SerialPort("/dev/ttyUSB0", {
   baudrate: 9600
 });
 
+
+
 // Turn TV on
-router.put('/on', function(req, res, next) {
-  serialPort.on("open", function () {
-    console.log('open');
-    serialPort.write("ka 01 01\n", function(err, results) {
-      console.log('err ' + err);
-      console.log('results ' + results);
-    });
-  });
+router.put('/power', function(req, res) {
+  console.log(req.body);
+  //  usbSerial.write("ka 01 01\n", function(err, results) {
+  //    console.log('err ' + err);
+  //    console.log('results ' + results);
+  //  });
   res.send("Turned it on");
 });
 
