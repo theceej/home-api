c96a1e2757e9de480b69d4798dfd0d9d6acae88e
theceej <theceej@users.noreply.github.com>
2015-01-26 21:21:30 +0000

    Trying sources

--- routes/amp.js
+++ routes/amp.js
@@ -4,8 +4,59 @@ var router = express.Router();
 var lirc = require('lirc_node');
 lirc.init();
 
+router.put('/state', function(req, res) {
+  console.log(req.body.length());
+  // Turn amp on and off using lirc
+  if('on' in req.body) {
+    if(req.body.on) {
+      lirc.irsend.send_once("amp", "powerOn", function() {
+        res.status(200).json({ 'success' : { 'amp/state/on' : true }});
+      });
+    } else {
+      lirc.irsend.send_once("amp", "powerOff", function() {
+        res.status(200).json({ 'success' : { 'amp/state/on' : false }});
+      });
+    }
+  }
+
+  // Change the amp source
+  if('source' in req.body) {
+    switch(req.body.source)
+    {
+      case 'chromecast':
+        lirc.irsend.send_once("amp", "vcrDvr", function() {
+          res.status(200).json({ 'success' : { 'amp/state/source' : 'chromecast' }});
+        });
+        break;
+      case 'ps3':
+        lirc.irsend.send_once("amp", "game", function() {
+          res.status(200).json({ 'success' : { 'amp/state/source' : 'ps3' }});
+        });
+        break;
+      case 'pi':
+        lirc.irsend.send_once("amp", "cd", function() {
+          res.status(200).json({ 'success' : { 'amp/state/source' : 'pi' }});
+        });
+        break;
+      case 'aux':
+        lirc.irsend.send_once("amp", "aux", function() {
+          res.status(200).json({ 'success' : { 'amp/state/source' : 'aux' }});
+        });
+        break;
+      case 'radio':
+        lirc.irsend.send_once("amp", "tuner", function() {
+          res.status(200).json({ 'success' : { 'amp/state/source' : 'radio' }});
+        });
+        break;
+      case default:
+        res.status(400).json({'error' : { 'description' : 'Invalid source specified, valid options are "chromecase", "ps3", "pi", "aux" or "radio"'}})
+    }
+  }
+
+});
+
 // Turn amp on and off using lirc
-router.put('/power', function(req, res) {
+router.put('/source', function(req, res) {
   if(typeof(req.body.on) === 'undefined') {
     res.status(400).json({ error : '"on" must be specified'})
   } else if(req.body.on) {
--- routes/tv.js
+++ routes/tv.js
@@ -6,16 +6,16 @@ var usbSerial = new SerialPort('/dev/ttyUSB0', { 'baudrate' : 9600 });
 
 // Turn TV on and off using serial port
 router.put('/state', function(req, res) {
-  if(req.body.on == true) {
-    usbSerial.write('ka 01 01\n', function(err, results) {
+  if(req.body.on === true) {
+    usbSerial.write('ka 01 01\n', function(err) {
       if(err) {
         res.status(500).json({ 'error' : { 'description' : 'Could not turn on TV: ' + err }});
       } else {
         res.status(200).json({ 'success' : { '/tv/state/on' : true }});
       }
     });
-  } else if(req.body.on == false) {
-    usbSerial.write('ka 01 00\n', function(err, results) {
+  } else if(req.body.on === false) {
+    usbSerial.write('ka 01 00\n', function(err) {
       if(err) {
         res.status(500).json({ 'error' : { 'description' : 'Could not turn off TV: ' + err }});
       } else {
