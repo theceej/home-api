b772225b762455451592d4ea52ce8718443aeec5
theceej <theceej@users.noreply.github.com>
2015-01-26 20:56:06 +0000

    Better error handling

--- routes/tv.js
+++ routes/tv.js
@@ -2,15 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var SerialPort = require('serialport').SerialPort
-var usbSerial = new SerialPort('/dev/ttyUSB0', {
-  baudrate: 9600
-});
+var usbSerial = new SerialPort('/dev/ttyUSB0', { 'baudrate' : 9600 });
 
 // Turn TV on and off using serial port
 router.put('/state', function(req, res) {
-  if(typeof(req.body.on) === 'undefined') {
-    res.status(400).json({ error : '"on" must be specified'})
-  } else if(req.body.on) {
+  if(req.body.on == true) {
     usbSerial.write('ka 01 01\n', function(err, results) {
       if(err) {
         res.status(500).json({ 'error' : { 'description' : 'Could not turn on TV: ' + err }});
@@ -18,15 +14,17 @@ router.put('/state', function(req, res) {
         res.status(200).json({ 'success' : { '/tv/state/on' : true }});
       }
     });
-  } else {
+  } else if(req.body.on == false) {
     usbSerial.write('ka 01 00\n', function(err, results) {
       if(err) {
         res.status(500).json({ 'error' : { 'description' : 'Could not turn off TV: ' + err }});
       } else {
-        res.status(200).json({ 'success' : { "/tv/state/on" : false }});
+        res.status(200).json({ 'success' : { '/tv/state/on' : false }});
       }
     });
-  }
+  } else {
+    res.status(400).json({ 'error' : { 'description' : 'Invalid option specified' }});
+  } else
 });
 
 module.exports = router;
